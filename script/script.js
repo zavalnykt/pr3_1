bb6388e59558f46cdad37dda88e7e43594abb48c
zavalnyk <107615023+zavalnykt@users.noreply.github.com>
2024-10-30 01:27:02 +0200

    Delete script directory

--- script/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('wordsButton').addEventListener('click', function(){
-    let wordsInput = document.getElementById('wordsInput').value;
-    let Pattern = wordsInput.trim().split(/\s+/);
-    let words = wordsInput.match(Pattern);
-    let wordsCout = words.length > 0 ? words.length : 0;
-  document.write('К-сть слів: ' + wordsCout);
-});
-
